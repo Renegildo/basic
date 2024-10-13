eb83f7ce5fa0f77775cb2443750fe34886517149
renegildo <renegildo@proton.me>
2024-10-13 20:06:15 -0300

    add gsap dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import Engagements from "./sections/Engagements";
 import Hero from "./sections/Hero";
 import Resume from "./sections/Resume";
+import { gsap } from "gsap/gsap-core";
+import { useGSAP } from "@gsap/react";
 
 const App = () => {
+  gsap.registerPlugin(useGSAP);
+
   return (
     <div>
       <Hero />
